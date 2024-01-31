a95de3f88223c0e95c2a53fe607efadfc2d687a7
to-lutz <tobias.lutz07@gmail.com>
2024-01-31 13:54:47 +0100

    Add JS Code to add products (to cart page for now)

--- static/cart.js
+++ static/cart.js
@@ -1,13 +1,45 @@
-fetch("./static/products.json").then((res) => {
-    if (!res.ok) {
-        throw new Error(`HTTP error! Status: ${res.status}`);
+const products = {
+    "test": {
+        "id": 1,
+        "name": "Testprodukt",
+        "description": "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,",
+        "price": 0.99,
+        "sale": true,
+        "priceNoSale": 1.49,
+        "imageSrc": "./static/productplaceholder.png",
+    },
+    "banane": {
+        "id": 2,
+        "name": "Banane",
+        "description": "Die beste Banane, die du jemals essen wirst! So knusprig wie noch nie!",
+        "price": 2.99,
+        "sale": false,
+        "priceNoSale": 2.99,
+        "imageSrc": "",
     }
-    return res.json();
-}).then((data) => {
-    for (product in data) {
-        let node = document.createElement("h1");
-            node.nodeValue = product;
-        console.log(node);
-        document.getElementById("body").appendChild(node);
+};  
+
+function displayProducts() {
+    for (productItem in products) {
+        let product = products[productItem];
+        let div = document.createElement("div");
+        div.className = "cart-item"
+        div.innerHTML = `<div class="cart-title">
+            <h5 class="cart-title-text">` + product.name + `</h5>
+            <img class="product-image" src="` + product.imageSrc + `"></img>
+        </div>
+        <p class="cart-description">` + product.description + `<p>
+        <div class="cart-footer">
+            <div>
+                <p class="price-text">Menge:&nbsp;</p>
+                <input class="cart-amount" type="number" value="1" min="0" max="99"/>
+            </div>
+            <div class="price-div">
+                <p class="price-text">Preis:&nbsp;<strong><p class="price-text" id="price">` + product.price + `</p>€</strong></p>
+            </div>
+        </div>`.trim();
+        document.getElementById("cart-items-wrapper").appendChild(div);
     }
-});
+}
+
+displayProducts();
