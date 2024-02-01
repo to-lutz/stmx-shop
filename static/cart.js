c5799c421386269f460514e967e250833060a0cc
to-lutz <tobias.lutz07@gmail.com>
2024-02-01 16:04:28 +0100

    Theme on cart page

--- static/cart.js
+++ static/cart.js
@@ -19,33 +19,75 @@ const products = {
     }
 };  
 
-// Light / Darkmode
-if (getCookie("stmx_mode") == "1") { // Lightmode
-    for (elem of document.querySelectorAll("#darkmode")) {
-        elem.style.display = "none";
-        elem.style.cursor = "default";
+function isSmallPage() {
+    return document.body.clientWidth <= 768;
+}
+
+lastWindowWidth = document.body.clientWidth;
+window.addEventListener("resize", (e) => {
+    refreshTheme();
+    if (lastWindowWidth <= 768 && document.body.clientWidth > 769) {
+        document.querySelector(".responsive-nav").style.display = "none";
+        ham.classList.remove("fa-x");
+        ham.classList.add("fa-bars");
     }
-    for (elem of document.querySelectorAll("*")) {
-        elem.classList.add("light-mode");
-    }
-} else if (getCookie("stmx_mode") == ""){ // No mode set, default: dark mode
-    setCookie("stmx_mode", "0", 365);
-    for (elem of document.querySelectorAll("#lightmode")) {
-        elem.style.display = "none";
-        elem.style.cursor = "default";
-    }
-    for (elem of document.querySelectorAll("*")) {
-        elem.classList.remove("light-mode");
-    }
-} else { // Darkmode
-    for (elem of document.querySelectorAll("#lightmode")) {
-        elem.style.display = "none";
-        elem.style.cursor = "default";
-    }
-    for (elem of document.querySelectorAll("*")) {
-        elem.classList.remove("light-mode");
+});
+
+function refreshTheme() {
+    // Light / Darkmode
+    if (getCookie("stmx_mode") == "1") { // Lightmode
+        for (elem of document.querySelectorAll("#darkmode")) {
+            elem.style.display = "none";
+            elem.style.cursor = "default";
+        }
+        for (elem of document.querySelectorAll("#lightmode")) {
+            elem.style.display = "inline-block";
+            elem.style.cursor = "pointer";
+        }
+        isSP = isSmallPage();
+        if (isSP) {
+            document.querySelectorAll(".bignav-modeswitch").forEach((e) => e.style.display="none");
+        } else {
+            document.querySelectorAll(".respnav-modeswitch").forEach((e) => e.style.display="none");
+        }
+
+        document.querySelectorAll(".navbar-logo").forEach((e) => {e.src = "./static/STMX-SHOP-WHITE-NOBG.png"});
+        document.querySelector(".navbar").style.backgroundColor="#fff";
+        document.querySelector(".responsive-nav").style.backgroundColor="#fff";
+        document.querySelector(".navbar").querySelectorAll("a").forEach((e) => e.style.color="#000");
+        document.querySelector(".responsive-nav").querySelectorAll("a").forEach((e) => e.style.color="#000");
+        document.querySelectorAll(".nav-hamburger").forEach((e) => e.style.color="#000");
+
+    } else if (getCookie("stmx_mode") == ""){ // No mode set, default: dark mode
+        setCookie("stmx_mode", "0", 365);
+    } else { // Darkmode
+        for (elem of document.querySelectorAll("#lightmode")) {
+            elem.style.display = "none";
+            elem.style.cursor = "default";
+        }
+        for (elem of document.querySelectorAll("#darkmode")) {
+            elem.style.display = "inline-block";
+            elem.style.cursor = "pointer";
+        }
+        isSP = isSmallPage();
+        if (isSP) {
+            document.querySelectorAll(".bignav-modeswitch").forEach((e) => e.style.display="none");
+        } else {
+            document.querySelectorAll(".respnav-modeswitch").forEach((e) => e.style.display="none");
+        }
+
+        document.querySelectorAll(".navbar-logo").forEach((e) => {e.src = "./static/STMX-SHOP-DARK-NOBG.png"});
+        document.querySelector(".navbar").style.backgroundColor="#444444";
+        document.querySelector(".responsive-nav").style.backgroundColor="#444444";
+        document.querySelector(".navbar").querySelectorAll("a").forEach((e) => e.style.color="#fff");
+        document.querySelector(".responsive-nav").querySelectorAll("a").forEach((e) => e.style.color="#fff");
+        document.querySelectorAll(".nav-hamburger").forEach((e) => e.style.color="#fff");
+
     }
 }
+
+document.querySelectorAll("#lightmode").forEach((e) => {e.addEventListener("click", (e) => {setCookie("stmx_mode", "0", 365); refreshTheme();})});
+document.querySelectorAll("#darkmode").forEach((e) => {e.addEventListener("click", (e) => {setCookie("stmx_mode", "1", 365); refreshTheme();})});
 
 const ham = document.querySelector('.nav-hamburger');
 ham.addEventListener("click", (e) => {
@@ -187,3 +229,4 @@ if (getCookie("stmx_cart_ids").length<2) setCookie("stmx_cart_ids", "1:2", 90);
 
 displayProducts();
 refreshTotalPrice();
+refreshTheme();
